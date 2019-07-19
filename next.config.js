04b9eef97f379f91b48c6f917df2d76211f72a08
haiming <1072950174@qq.com>
2019-07-19 19:45:31 +0800

    完成了打包bug修复

--- next.config.js
+++ next.config.js
@@ -5,11 +5,9 @@ const path = require('path')
 const extensions = ['*', '.web.tsx', '.web.ts', '.web.js', '.js', '.jsx', '.json', '.scss', '.jpg', '.png']
 module.exports = withSCSS(withImages(withCSS({
   webpack: (config) => {
-    config.resolve.alias = {
-      style: path.resolve(__dirname, 'style'),
-      src: path.resolve(__dirname, 'src'),
-      routes: path.resolve(__dirname, 'routes'),
-    }
+    config.resolve.alias.style=path.resolve(__dirname, 'style')
+    config.resolve.alias.src=path.resolve(__dirname, 'src')
+    config.resolve.alias.routes=path.resolve(__dirname, 'routes')
     config.resolve.extensions = extensions
     config.module.rules.push({
       test: /\.(eot|woff|woff2|ttf|svg)$/,
